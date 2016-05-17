cbc45b5a9441d979a718885a4c84b643b08b61f5
Rahul U Nair <rahulunair@gmail.com>
2016-05-17 10:52:29 -0500

    Added endpoint to be tested parameter as well
    Fixed some bugs as well in the client, also edited the config
    to set endpoint urls.

--- vAPI/client.py
+++ vAPI/client.py
@@ -10,27 +10,29 @@ def authenticate(url, username=None, password=None):
     url = '{0}/tokens'.format(url)
 
     kwargs = {}
+    headers = {}
     kwargs["username"] = username
     kwargs["password"] = password
+    headers["Content-Type"] = "Application/json"
     data = {"auth": {"passwordCredentials": kwargs}}
-    r = requests.post(url, data=json.dumps(data))
+
+    r = requests.post(url, data=json.dumps(data), headers=headers)
 
     if not r.ok:
         raise Exception("Failed to authenticate")
 
-    if r.entity is None:
-        raise Exception("Failed to parse Auth response Body")
-    return r
+    return json.loads(r.text)
 
 
 def get_token():
 
-    username = syntribos.extensions.vAPI.config.UserConfig(
-        section_name="credentials").url
-    password = syntribos.extensions.vAPI.config.UserConfig(
-        section_name="credentials").username
     url = syntribos.extensions.vAPI.config.UserConfig(
-        section_name="credentials").password
+        section_name="vAPI").url
+    username = syntribos.extensions.vAPI.config.UserConfig(
+        section_name="vAPI").username
+    password = syntribos.extensions.vAPI.config.UserConfig(
+        section_name="vAPI").password
 
     access_data = authenticate(url, username, password)
+    print "ENTERED GET_TOKEN_BLOCK, ACCESS DATA IS:: ", access_data
     return access_data["access"]["token"]["id"]
